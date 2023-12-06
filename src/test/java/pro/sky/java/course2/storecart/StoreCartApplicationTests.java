56ca8fa39f7cde7e549d227a3d96339441c99bd9
SkynetBuilder <vlad.gulenkov@gmail.com>
2023-12-06 21:26:31 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/storecart/StoreCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.storecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
